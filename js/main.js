8ca5b9c021e1adf59a5911811c542c8a2b85b170
andy19910102 <andy19910102@gmail.com>
2020-08-23 00:38:46 +0800

    Update content

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 // 網頁的主程式寫在這裡
+// .ad-toggle-btn click event
+$(".ad-toggle-btn").click(function () {
+    console.log('[.ad-toggle-btn被點擊了]');
+
+});
+
+// .navbar .nav-link click event
+$(".navbar .nav-link").click(function () {
+    console.log('[導覽列按鈕被點擊了]', this);
+
+});
+
