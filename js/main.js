cfed6b3d16dcbd78bd049563042c6820c9a14e11
andy19910102 <andy19910102@gmail.com>
2021-03-22 17:58:22 +0800

    Update content

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
 // 網頁的主程式寫在這裡
-// .ad-toggle-btn click event
+// 綁定 .ad-toggle-btn 的點擊事件
 $(".ad-toggle-btn").click(function () {
-    console.log('[.ad-toggle-btn被點擊了]');
+    console.log("[.ad-toggle-btn被點擊了]");
 
 });
 
-// .navbar .nav-link click event
+// 綁定 #removeSideBoxBtn 的點擊事件
+$("#removeSideBoxBtn").click(function () {
+    console.log("[#removeSideBoxBtn被點擊了]")
+
+});
+
+// 綁定 .navbar 裡面 .nav-link 的點擊事件
 $(".navbar .nav-link").click(function () {
-    console.log('[導覽列按鈕被點擊了]', this);
+    console.log("[導覽列按鈕被點擊了]", this);
+    // 取得滑動目標
+
+    // 取得滑動目標的所在座標
+
+    // 設定動畫時間
+
+    // 執行滑動動畫
 
 });
-
